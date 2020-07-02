5efd14eea8a8c42457747ec7d8f31856cf7fc492
JuanJoseGomezR <juangomez0149@gmail.com>
2020-07-02 01:32:11 +0000

    [Uploading] Test Basemodel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,91 +1,227 @@
 #!/usr/bin/python3
-""" doctest unittest Base Model"""
+"""
+BaseModel unittest Module
+"""
 import unittest
 import pep8
-from models.base_model import BaseModel
-from models import storage
-from models.engine.file_storage import FileStorage
+import models
+import inspect
 import os
+from time import sleep
+import json
+from datetime import datetime
+CurrentTestClass = models.base_model.BaseModel
+current_module = models.base_model
+file_name = os.path.basename(__file__)
+current_path = "tests/test_models/{}".format(file_name)
+test_path = "models/{}".format(file_name[5:])
+cls_attr = ['id', 'created_at', 'updated_at']
 
 
-class TestBase(unittest.TestCase):
-    """ test Base Model"""
+class TestBaseModel(unittest.TestCase):
+    """TestBaseModel class"""
 
-    def test_pep8(self):
-        """ test pep8 """
-        style = pep8.StyleGuide(quiet=True)
-        file_base_model = "models/base_model.py"
-        file_test_base_model = "tests/test_models/test_base_model.py"
-        check = style.check_files([file_base_model, file_test_base_model])
-        self.assertNotEqual(check.total_errors, 0,
-                         "Found code style errors (and warning).")
-
-    @classmethod
-    def setUpClass(cls):
-        """ first set up
-        check = style.check_files([file_base_model, file_test_base_model])
-        """
-        cls.ins = BaseModel()
-
-    @classmethod
-    def teardown(cls):
-        """ final statement """
-        del cls.ins
+    def setUp(self):
+        """Executes before each test"""
         try:
             os.remove("file.json")
-        except:
+        except IOError:
             pass
 
-    def test_BaseModeldoc(self):
-        """ test base model documentation
-        self.assertNotEqual(len(models.__doc__), 0)
-        self.assertNotEqual(len(models.base_model.__doc__), 0)
-        """
-        self.assertNotEqual(len(BaseModel.__doc__), 0)
-        self.assertIsNotNone(BaseModel.__init__.__doc__)
-        self.assertIsNotNone(BaseModel.__str__.__doc__)
-        self.assertIsNotNone(BaseModel.save.__doc__)
-        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+    def tearDown(self):
+        """Executes when test finish"""
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
 
-    def test_BaseModelAttr(self):
-        """ test basemodel attributes """
-        self.assertEqual(hasattr(self.ins, "id"), True)
-        self.assertEqual(hasattr(self.ins, "created_at"), True)
-        self.assertEqual(hasattr(self.ins, "updated_at"), True)
+    def test_pep8(self):
+        """Check PEP8 style"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(
+                                       [
+                                        test_path,
+                                        current_path
+                                       ]
+                                      )
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def test_isinstance(self):
-        self.assertTrue(isinstance(self.ins, BaseModel))
+    def test_docstring(self):
+        """Checks the docstring documentation"""
+        self.assertTrue(len(current_module.__doc__) >= 1)
+        self.assertTrue(len(CurrentTestClass.__doc__) >= 1)
+        methods = inspect.getmembers(
+                                     CurrentTestClass,
+                                     inspect.isfunction
+                                    )
+        for m in methods:
+            self.assertTrue(len(m[1].__doc__) >= 1)
 
-    def test_save_updated_at_created_at(self):
-        self.ins.save()
-        self.assertNotEqual(self.ins.created_at, self.ins.updated_at)
-        dummy = BaseModel()
-        my_id = dummy.id
-        dummy.name = "Juan"
-        dummy.save()
-        storage.reload()
-        my_objs = storage.all()["BaseModel.{}".format(my_id)]
-        self.assertTrue(hasattr(my_objs, "name"))
-        self.assertTrue(my_objs.name == "Juan")
-        self.assertTrue(os.path.exists('file.json'))
+    def test_base_model_single_instance(self):
+        """Test a single instance"""
+        cti1 = CurrentTestClass()
+        for attr in cls_attr:
+            self.assertTrue(hasattr(cti1, attr))
+        self.assertEqual(type(cti1.id), str)
+        self.assertEqual(type(cti1.created_at), datetime)
+        self.assertEqual(type(cti1.updated_at), datetime)
+        bm_funcs = inspect.getmembers(cti1)
+        func_names = [fn[0] for fn in bm_funcs]
+        self.assertIn('save', func_names)
+        self.assertIn('to_dict', func_names)
 
-    def test_dict(self):
-        dicto = self.ins.to_dict()
-        self.assertTrue(dicto.get("__class__"))
-        self.assertTrue(type(dicto) is dict)
-        self.assertTrue("to_dict" in dir(self.ins))
+    def test_two_instances(self):
+        """Test two instances"""
+        cti1 = CurrentTestClass()
+        cti2 = CurrentTestClass()
+        self.assertNotEqual(cti1.id, cti2.id)
+        self.assertNotEqual(cti1.created_at, cti2.created_at)
+        self.assertLess(cti1.created_at, cti2.created_at)
+        self.assertNotEqual(cti1.updated_at, cti2.updated_at)
+        self.assertLess(cti1.updated_at, cti2.updated_at)
+        self.assertIsNot(cti1, cti2)
 
-    def test_var_storage(self):
-        my_objs = storage.all()
+    def test_str_method(self):
+        """Test str method"""
+        cti1 = CurrentTestClass()
+        cti1.id = "1234"
+        cti1.name = "Holberton"
+        cti1.my_number = 89
+        cti1_dict = {
+                    'id': cti1.id,
+                    'created_at': repr(cti1.created_at),
+                    'updated_at': repr(cti1.updated_at),
+                    'name': 'Holberton',
+                    'my_number': 89,
+                   }
+        cls_id = "[{}] (1234)".format(CurrentTestClass.__name__)
+        self.assertIn(cls_id, cti1.__str__())
+        for k, v in cti1_dict.items():
+            self.assertIn(str(k), cti1.__str__())
+            self.assertIn(str(v), cti1.__str__())
 
-        self.assertTrue(type(my_objs) is dict)
-        self.assertTrue(isinstance(storage, FileStorage))
+    def test_with_kwarg(self):
+        """Test instance with **kwarg"""
+        date = datetime.now().isoformat()
+        cti1 = CurrentTestClass(id='12345', created_at=date, updated_at=date)
+        date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+        self.assertEqual(cti1.id, "12345")
+        self.assertEqual(str(cti1.created_at), str(date))
+        self.assertEqual(str(cti1.updated_at), str(date))
 
-    def test_reload(self):
-        self.ins.save()
-        storage.reload()
-        my_dict = storage.all()
-        self.assertTrue(len(my_dict) != 0)
+    def test_with_more_kwarg(self):
+        """Test instance with extra **kwarg"""
+        date = datetime.now().isoformat()
+        cti1 = CurrentTestClass(id='12345', created_at=date, updated_at=date,
+                                name='Santiago', age=28)
+        date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+        self.assertEqual(cti1.id, "12345")
+        self.assertEqual(str(cti1.created_at), str(date))
+        self.assertEqual(str(cti1.updated_at), str(date))
+        self.assertEqual(cti1.name, "Santiago")
+        self.assertIs(type(cti1.name), str)
+        self.assertEqual(cti1.age, 28)
+        self.assertIs(type(cti1.age), int)
 
-if __name__ == '__main__':
+    def test_with_kwarg_class(self):
+        """Test instance __class__ attr **kwarg"""
+        date = datetime.now().isoformat()
+        cti1 = CurrentTestClass(id='12345', created_at=date, updated_at=date,
+                                __class__="AnotherClass")
+        date = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
+        self.assertEqual(cti1.id, "12345")
+        self.assertEqual(str(cti1.created_at), str(date))
+        self.assertEqual(str(cti1.updated_at), str(date))
+        self.assertNotEqual(cti1.__class__, "AnotherClass")
+        self.assertNotEqual(cti1.__class__, CurrentTestClass.__name__)
+
+    def test_with_arg(self):
+        """Test with instance args"""
+        date = datetime.now().isoformat()
+        cti1 = CurrentTestClass('12345', date, date)
+        self.assertNotEqual(cti1.id, '12345')
+        self.assertNotEqual(cti1.created_at, date)
+        self.assertNotEqual(cti1.updated_at, date)
+
+    def test_kwargs_None(self):
+        """Test **kwargs with None"""
+        with self.assertRaises(TypeError):
+            CurrentTestClass(id=None, created_at=None, updated_at=None)
+        cti1 = CurrentTestClass(None)
+        self.assertNotIn(None, cti1.__dict__)
+
+    def test_save(self):
+        """Test save() function"""
+        cti1 = CurrentTestClass()
+        old_date = cti1.updated_at
+        sleep(1)
+        cti1.save()
+        self.assertNotEqual(old_date, cti1.updated_at)
+        old_date2 = cti1.updated_at
+        sleep(1)
+        cti1.save()
+        self.assertNotEqual(old_date2, cti1.updated_at)
+
+    def test_save_args(self):
+        """Test save function with args"""
+        cti = CurrentTestClass()
+        with self.assertRaises(TypeError):
+            cti.save(1)
+        with self.assertRaises(TypeError):
+            cti.save(None)
+
+    def test_is_saving(self):
+        """Test if it is saving in file.json"""
+        cti1 = CurrentTestClass()
+        sleep(1)
+        cti1.save()
+        with open('file.json') as f:
+            cti1_list = json.load(f)
+        key = "{}.{}".format(CurrentTestClass.__name__, cti1.id)
+        self.assertIn(key, cti1_list)
+        self.assertDictEqual(cti1.to_dict(), cti1_list[key])
+
+    def test_to_dict(self):
+        """Test to_dict function"""
+        cti1 = CurrentTestClass()
+        cti1_dict = cti1.to_dict()
+        self.assertEqual(type(cti1_dict), dict)
+        self.assertIn('updated_at', cti1_dict)
+        self.assertIn('created_at', cti1_dict)
+        self.assertIn('__class__', cti1_dict)
+        self.assertNotEqual(cti1_dict, cti1.__dict__)
+
+    def test_to_dict_args(self):
+        """Test to_dict function with arguments"""
+        cti = CurrentTestClass()
+        with self.assertRaises(TypeError):
+            cti.to_dict(1)
+        with self.assertRaises(TypeError):
+            cti.to_dict(None)
+
+    def test_to_dict_new_attributes(self):
+        """Test to_dict fucntion when new attributes are set"""
+        cti1 = CurrentTestClass()
+        cti1.name = 'Alejandro'
+        cti1.age = 21
+        cti1_dict = cti1.to_dict()
+        self.assertIn('name', cti1_dict)
+        self.assertIn('age', cti1_dict)
+        self.assertEqual('Alejandro', cti1_dict['name'])
+        self.assertEqual(21, cti1_dict['age'])
+
+    def test_to_dict_datetime(self):
+        """Test to_dict function with datetime attributes"""
+        cti1 = CurrentTestClass()
+        old_created = cti1.created_at
+        old_updated = cti1.updated_at
+        cti1_dict = cti1.to_dict()
+        self.assertEqual(type(cti1_dict['created_at']), str)
+        self.assertEqual(type(cti1_dict['updated_at']), str)
+        self.assertNotEqual(cti1_dict['created_at'], old_created)
+        self.assertNotEqual(cti1_dict['updated_at'], old_updated)
+
+
+if __name__ == "__main__":
     unittest.main()
