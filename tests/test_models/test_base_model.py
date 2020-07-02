ac6766f17a203fd6e583fc6a87b6d9f557e7e69f
FabianMosquera <rfmq9530@gmail.com>
2020-07-02 01:01:54 +0000

    add others tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,51 +1,91 @@
-#!/usr/bin/pyton3
-"""Unittest BaseModel class"""
+#!/usr/bin/python3
+""" doctest unittest Base Model"""
 import unittest
-import os
+import pep8
 from models.base_model import BaseModel
+from models import storage
+from models.engine.file_storage import FileStorage
+import os
 
 
-class test_base_model(unittest.TestCase):
+class TestBase(unittest.TestCase):
+    """ test Base Model"""
 
-    def setUp(self):
-        """ setup test """
-        self.base = BaseModel()
+    def test_pep8(self):
+        """ test pep8 """
+        style = pep8.StyleGuide(quiet=True)
+        file_base_model = "models/base_model.py"
+        file_test_base_model = "tests/test_models/test_base_model.py"
+        check = style.check_files([file_base_model, file_test_base_model])
+        self.assertNotEqual(check.total_errors, 0,
+                         "Found code style errors (and warning).")
 
-    def tearDown(self):
-        """ teardown test """
-        del self.base
+    @classmethod
+    def setUpClass(cls):
+        """ first set up
+        check = style.check_files([file_base_model, file_test_base_model])
+        """
+        cls.ins = BaseModel()
+
+    @classmethod
+    def teardown(cls):
+        """ final statement """
+        del cls.ins
         try:
             os.remove("file.json")
-        except FileNotFoundError:
+        except:
             pass
 
-    def test_init(self):
-        """ init test """
-        self.assertTrue(isinstance(self.base, BaseModel))
+    def test_BaseModeldoc(self):
+        """ test base model documentation
+        self.assertNotEqual(len(models.__doc__), 0)
+        self.assertNotEqual(len(models.base_model.__doc__), 0)
+        """
+        self.assertNotEqual(len(BaseModel.__doc__), 0)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
+        self.assertIsNotNone(BaseModel.__str__.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
 
-    def test_atritt(self):
-        """ atritt test """
-        self.assertTrue(hasattr(BaseModel, "__init__"))
-        self.assertTrue(hasattr(BaseModel, "to_dict"))
-        self.assertTrue(hasattr(BaseModel, "save"))
+    def test_BaseModelAttr(self):
+        """ test basemodel attributes """
+        self.assertEqual(hasattr(self.ins, "id"), True)
+        self.assertEqual(hasattr(self.ins, "created_at"), True)
+        self.assertEqual(hasattr(self.ins, "updated_at"), True)
 
-    def test_to_dict(self):
-        """ test to dict """
-        base = BaseModel()
-        base.name = "Holberton"
-        base.age = 89
-        convert = base.to_dict()
-        self.assertEqual(convert["id"], base.id)
-        self.assertEqual(convert["name"], base.name)
-        self.assertEqual(convert["age"], base.age)
-        self.assertEqual(convert["updated_at"], base.updated_at.isoformat())
-        self.assertEqual(convert["created_at"], base.created_at.isoformat())
-        self.assertEqual(convert["__class__"], type(base).__name__)
+    def test_isinstance(self):
+        self.assertTrue(isinstance(self.ins, BaseModel))
 
-    def tets_save(self):
-        """ save test """
-        self.base.save()
-        self.assertNotEqual(self.base.created_at, self.base.updated_at)
+    def test_save_updated_at_created_at(self):
+        self.ins.save()
+        self.assertNotEqual(self.ins.created_at, self.ins.updated_at)
+        dummy = BaseModel()
+        my_id = dummy.id
+        dummy.name = "Juan"
+        dummy.save()
+        storage.reload()
+        my_objs = storage.all()["BaseModel.{}".format(my_id)]
+        self.assertTrue(hasattr(my_objs, "name"))
+        self.assertTrue(my_objs.name == "Juan")
+        self.assertTrue(os.path.exists('file.json'))
 
-if __name__ == "__main__":
+    def test_dict(self):
+        dicto = self.ins.to_dict()
+        self.assertTrue(dicto.get("__class__"))
+        self.assertTrue(type(dicto) is dict)
+        self.assertTrue("to_dict" in dir(self.ins))
+
+    def test_var_storage(self):
+        my_objs = storage.all()
+
+        self.assertTrue(type(my_objs) is dict)
+        self.assertTrue(isinstance(storage, FileStorage))
+
+    def test_reload(self):
+        self.ins.save()
+        storage.reload()
+        my_dict = storage.all()
+        self.assertTrue(len(my_dict) != 0)
+
+if __name__ == '__main__':
     unittest.main()
